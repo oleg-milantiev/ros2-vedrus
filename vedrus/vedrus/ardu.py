4951aab66c35b062ff0026b7f1be3c6fd880f2ae
Oleg Milantiev <oleg@milantiev.com>
2024-02-09 03:51:41 +0300

    Auto detect ardu sides. Keyboard test controller

--- vedrus/vedrus/ardu.py
+++ vedrus/vedrus/ardu.py
@@ -9,56 +9,49 @@ import rclpy
 import serial
 from rclpy.node import Node
 
-from std_msgs.msg import UInt8, UInt16, Float32
+from std_msgs.msg import UInt8, UInt16, Float32, String
 from sensor_msgs.msg import Temperature, RelativeHumidity
 from vedrus_interfaces.msg import Motor, MotorMove
 
 
-ser = serial.Serial('/dev/ttyUSB0', 115200, rtscts=True)
-
-# todo move to argv
-side = 'left'
-
 class VedrusArduNode(Node):
     buf = ''
-    count = 0
+    side = None
+    start = None
 
     def move_motor(self, msg):
         buf = [ord('c')]
         buf.append(ord('b') if msg.breaking else ord('m'))
-        buf.append(ord('f') if msg.forward else ord('r'))
+
+        if self.side == 'left':
+            buf.append(ord('f') if msg.forward else ord('r'))
+        else:
+            buf.append(ord('r') if msg.forward else ord('f'))
+
         buf.append(msg.power)
 
         print(msg)
-#        print(buf)
-        ser.write(buf)
+        self.ser.write(buf)
 
     def __init__(self):
-        super().__init__('vedrus_'+ side +'_node')
+        super().__init__('vedrus_ardu')
 
-        self.subscription = self.create_subscription(
-            MotorMove,
-            'vedrus/'+ side +'/move',
-            self.move_motor,
-            10)
-        self.subscription  # prevent unused variable warning
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('device', rclpy.Parameter.Type.STRING),
+            ]
+        )
 
-        self.publisher
-        self.publisher_temperature = self.create_publisher(Temperature, 'vedrus/'+ side +'/temperature', 10)
-        self.publisher_humidity = self.create_publisher(RelativeHumidity, 'vedrus/'+ side +'/humidity', 10)
-        self.publisher_sonar1 = self.create_publisher(Float32, 'vedrus/'+ side +'/front/sonar', 10)
-        self.publisher_sonar2 = self.create_publisher(Float32, 'vedrus/'+ side +'/rear/sonar', 10)
-
-        self.publisher_motor_front = self.create_publisher(Motor, 'vedrus/'+ side +'/front/motor', 10)
-        self.publisher_motor_rear = self.create_publisher(Motor, 'vedrus/'+ side +'/rear/motor', 10)
+        self.ser = serial.Serial(self.get_parameter('device').get_parameter_value().string_value, 115200, rtscts=True)
 
         self.timer = self.create_timer(0.1, self.read_serial)
         self.timer  # prevent unused variable warning
 
     def read_serial(self):
-        if ser.in_waiting > 0:
+        if self.ser.in_waiting > 0:
             # b'141,65,21.24,45.52,735.97,22,END\r\nVEDRUS,0,0,1,1,140,66,21.24,45.52,735.97,27,END\r\nVEDRUS,15,18,1,1,140,66,21.24,45.50,735.97,42,END\r\nVEDRUS,11,0,1,1,140,65,21.24,45.50,735.97,27,END\r\nVEDRUS,0,0,1,1,'
-            self.buf += ser.read(ser.in_waiting).decode('utf-8')
+            self.buf += self.ser.read(self.ser.in_waiting).decode('utf-8')
 
             #['141,65,21.24,45.52,735.97,22,END', 'VEDRUS,312,299,1,1,141,66,21.29,45.39,736.10,162,END', 'VEDRUS,140,134,1,1,141,67,21.29,45.39,736.10,128,END', 'VEDRU']
             lines = self.buf.split('\r\n')
@@ -67,7 +60,34 @@ class VedrusArduNode(Node):
             for line in lines:
                 div = line.split(',')
 
-                if len(div) == 12 and (div[0] == 'VEDL' or div[0] == 'VEDR') and div[ len(div) - 1 ] == 'END':
+                if len(div) == 12 and (div[0] == 'VEDR' or div[0] == 'VEDL') and div[ len(div) - 1 ] == 'END':
+                    if self.side is None:
+                        self.side = 'left' if div[0] == 'VEDL' else 'right'
+                        self.start = div[0]
+
+                        self.subscription = self.create_subscription(
+                            MotorMove,
+                            'vedrus/'+ self.side +'/move',
+                            self.move_motor,
+                            10)
+                        self.subscription  # prevent unused variable warning
+
+                        self.publisher_temperature = self.create_publisher(Temperature, 'vedrus/'+ self.side +'/temperature', 10)
+                        self.publisher_humidity = self.create_publisher(RelativeHumidity, 'vedrus/'+ self.side +'/humidity', 10)
+                        self.publisher_sonar1 = self.create_publisher(Float32, 'vedrus/'+ self.side +'/front/sonar', 10)
+                        self.publisher_sonar2 = self.create_publisher(Float32, 'vedrus/'+ self.side +'/rear/sonar', 10)
+
+                        self.publisher_motor_front = self.create_publisher(Motor, 'vedrus/'+ self.side +'/front/motor', 10)
+                        self.publisher_motor_rear = self.create_publisher(Motor, 'vedrus/'+ self.side +'/rear/motor', 10)
+
+                        self.publisher_raw = self.create_publisher(String, 'vedrus/'+ self.side +'/raw', 10)
+
+                    if self.side is not None:
+                        msg = String()
+                        msg.data = line
+                        self.publisher_raw.publish(msg)
+
+                if self.side is not None and len(div) == 12 and div[0] == self.start and div[ len(div) - 1 ] == 'END':
 
                     try:
                         '''
@@ -102,11 +122,11 @@ class VedrusArduNode(Node):
                         self.publisher_humidity.publish(msg)
 
                         msg = Float32()
-                        msg.data = float(div[5])
+                        msg.data = float(div[5]) / 57.
                         self.publisher_sonar1.publish(msg)
 
                         msg = Float32()
-                        msg.data = float(div[6])
+                        msg.data = float(div[6]) / 57.
                         self.publisher_sonar2.publish(msg)
 
                         msg = Motor()
@@ -119,14 +139,11 @@ class VedrusArduNode(Node):
                         msg.current = float(div[2])
                         self.publisher_motor_rear.publish(msg)
 
-                        #print('c='+ str(self.count) + ', lines='+ str(len(lines)))
-                        #self.count += 1
-
                     except ValueError:
                         print('error')
                         pass
 
-            self.buf = '' if len(div) == 12 and (div[0] == 'VEDL' or div[0] == 'VEDR') and div[ len(div) - 1 ] == 'END' else lines[ len(lines) - 1 ]
+            self.buf = '' if self.side is not None and len(div) == 12 and div[0] == self.start and div[ len(div) - 1 ] == 'END' else lines[ len(lines) - 1 ]
 
 
 def main(args=None):
--- vedrus/vedrus/test.py
+++ vedrus/vedrus/test.py
@@ -1,37 +1,72 @@
 import rclpy
+import getch
 from rclpy.node import Node
 
 from vedrus_interfaces.msg import MotorMove
 
 
-class VedrusLeftNode(Node):
-    power = 0
+class VedrusTestNode(Node):
+
+    left = 0
+    right = 0
 
     def __init__(self):
-        super().__init__('vedrus_right_node')
+        super().__init__('vedrus_test')
 
-        self.timer = self.create_timer(1, self.publish_motor)
-        self.timer  # prevent unused variable warning
+        self.publisher_left = self.create_publisher(MotorMove, 'vedrus/left/move', 10)
+        self.publisher_right = self.create_publisher(MotorMove, 'vedrus/right/move', 10)
 
-        self.publisher_ = self.create_publisher(MotorMove, 'vedrus/left/move', 10)
+        while (1):
+            wasLeft = self.left
+            wasRight = self.right
+
+            match getch.getch():
+                case 'q':
+                    self.left += 5
+                case 'a':
+                    self.left -= 5
+                case 'w':
+                    self.right += 5
+                case 's':
+                    self.right -= 5
+
+            if wasLeft != self.left:
+                msg = MotorMove()
+                msg.breaking = False
+                msg.forward = self.left >= 0
+                msg.power = abs(self.left) * 1 # WTF!!!!??? :)))
+
+                print('Left')
+                print(msg)
+                self.publisher_left.publish(msg)
+
+            if wasRight != self.right:
+                msg = MotorMove()
+                msg.breaking = False
+                msg.forward = self.right >= 0
+                msg.power = abs(self.right) * 1 # WTF!!!!??? :)))
+
+                print('Right')
+                print(msg)
+                self.publisher_right.publish(msg)
+
 
     def publish_motor(self):
         msg = MotorMove()
         msg.breaking = False
         msg.forward = True
-        msg.power = self.power * 1 # WTF!!!!??? :)))
+        msg.power = self.powers[self.power] * 1 # WTF!!!!??? :)))
 
-        self.power += 1
-        self.publisher_.publish(msg)
+        self.publisher_left.publish(msg)
+        self.publisher_right.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    vedrus_left_node = VedrusLeftNode()
-#    vedrus_left_node.read_serial()
-    rclpy.spin(vedrus_left_node)
+    vedrus_test_node = VedrusTestNode()
+    rclpy.spin(vedrus_test_node)
 
-    vedrus_left_node.destroy_node()
+    vedrus_test_node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
