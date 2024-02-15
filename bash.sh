166c81e2207ad4f257a1834fb21d13ca4dfa05f2
Oleg Milantiev <oleg@milantiev.com>
2024-02-15 20:45:35 +0300

    Intel RealSense docker - working image

--- bash.sh
+++ bash.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 docker run -it --rm --privileged \
-    -v /dev/ttyUSB0:/dev/ttyUSB0 \
-    -v /dev/ttyUSB1:/dev/ttyUSB1 \
-    -v /dev/ttyUSB2:/dev/ttyUSB2 \
+    -v /dev:/dev \
     -v /root/ros2-vedrus/launch:/opt/ros/iron/launch \
     -v /root/ros2-vedrus/vedrus:/opt/ros/iron/src/vedrus \
     -v /root/ros2-vedrus/vedrus_interfaces:/opt/ros/iron/src/vedrus_interfaces \
