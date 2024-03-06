3bf71bf98fbf68272a45172112db3a3a10c89201
Oleg Milantiev <oleg@milantiev.com>
2024-03-06 12:03:30 +0000

    Move Ardu serial read to asycn. Publish stable 5pfs

--- vedrus/vedrus/ardu.py
+++ vedrus/vedrus/ardu.py
@@ -8,6 +8,8 @@ import sys
 import rclpy
 import serial
 from rclpy.node import Node
+import time
+import threading
 
 from std_msgs.msg import UInt8, UInt16, Float32, String
 from sensor_msgs.msg import Temperature, RelativeHumidity
@@ -15,7 +17,6 @@ from vedrus_interfaces.msg import Motor, MotorMove, Sonar
 
 
 class VedrusArduNode(Node):
-	buf = ''
 	side = None
 	start = None
 
@@ -44,100 +45,101 @@ class VedrusArduNode(Node):
 
 		self.ser = serial.Serial(self.get_parameter('device').get_parameter_value().string_value, 115200, rtscts=True)
 
-		self.timer = self.create_timer(0.1, self.read_serial)
+		self.serial_thread = threading.Thread(target=self.serial_reader)
+		self.serial_thread.start()
 
-	def read_serial(self):
-		if self.ser.in_waiting > 0:
+		self.timer = self.create_timer(0.2, self.timer_publish)
+
+		self.get_logger().info('Node initialized')
+
+	line = None
+
+	def timer_publish(self):
+		if self.line is not None:
+			div = self.line.split(',')
+			self.line = None
+
+			if len(div) == 14 and (div[0] == 'VEDR' or div[0] == 'VEDL') and div[ len(div) - 1 ] == 'END':
+				if self.side is None:
+					self.side = 'left' if div[0] == 'VEDL' else 'right'
+					self.start = div[0]
+
+					self.subscription = self.create_subscription(
+						MotorMove,
+						'vedrus/'+ self.side +'/move',
+						self.move_motor,
+						10)
+
+					self.publisher_temperature = self.create_publisher(Temperature, 'vedrus/'+ self.side +'/temperature', 10)
+					self.publisher_humidity = self.create_publisher(RelativeHumidity, 'vedrus/'+ self.side +'/humidity', 10)
+
+					self.publisher_sonar = self.create_publisher(Sonar, 'vedrus/sonar', 10)
+
+					self.publisher_motor = self.create_publisher(Motor, 'vedrus/motor', 10)
+
+					self.publisher_raw = self.create_publisher(String, 'vedrus/'+ self.side +'/raw', 10)
+
+			if self.side is not None and len(div) == 14 and div[0] == self.start and div[ len(div) - 1 ] == 'END':
+
+				try:
+					#[0'VEDR', 1'196.88', 2'210.56', 3'0', 4'0', 5'2882', 6'-1', 7'0', 8'0', 9'0', 10'205.50', 11'0', 12'0', 'END']
+					stamp = self.get_clock().now().to_msg()
+
+					msg = Temperature()
+					msg.temperature = float(div[7])
+					msg.variance = 0.
+					self.publisher_temperature.publish(msg)
+
+					msg = RelativeHumidity()
+					msg.relative_humidity = float(div[8])
+					msg.variance = 0.
+					self.publisher_humidity.publish(msg)
+
+					msg = Sonar()
+					msg.header.frame_id = self.side +'_side'
+					msg.header.stamp = stamp
+					msg.range = -1. if div[5] == '-1' else float(div[5]) / 57.
+					msg.azimuth = 90. if self.side == 'right' else 270.
+					self.publisher_sonar.publish(msg)
+
+					msg = Sonar()
+					msg.header.frame_id = self.side +'_rear'
+					msg.header.stamp = stamp
+					msg.range = -1. if div[6] == '-1' else float(div[6]) / 57.
+					msg.azimuth = 180.
+					self.publisher_sonar.publish(msg)
+
+					msg = Motor()
+					msg.header.frame_id = self.side +'_front'
+					msg.header.stamp = stamp
+					msg.tick = int(div[3])
+					msg.current = float(div[1])
+					msg.power = int(div[11])
+					msg.forward = div[12] == '1'
+					self.publisher_motor.publish(msg)
+
+					msg = Motor()
+					msg.header.frame_id = self.side +'_rear'
+					msg.header.stamp = stamp
+					msg.tick = int(div[4])
+					msg.current = float(div[2])
+					msg.power = int(div[11])
+					msg.forward = div[12] == '1'
+					self.publisher_motor.publish(msg)
+
+				except ValueError:
+					print('ValueError')
+					pass
+
+	def serial_reader(self):
+		self.get_logger().info('Serial threading started')
+
+		while rclpy.ok():
 			try:
-				self.buf += self.ser.read(self.ser.in_waiting).decode('ascii')
+				self.line = self.ser.readline().decode('ascii').strip()
+
 			except:
-				return
-
-			lines = self.buf.split('\r\n')
-			div = []
-
-			for line in lines:
-				div = line.split(',')
-
-				if len(div) == 14 and (div[0] == 'VEDR' or div[0] == 'VEDL') and div[ len(div) - 1 ] == 'END':
-					if self.side is None:
-						self.side = 'left' if div[0] == 'VEDL' else 'right'
-						self.start = div[0]
-
-						self.subscription = self.create_subscription(
-							MotorMove,
-							'vedrus/'+ self.side +'/move',
-							self.move_motor,
-							10)
-
-						self.publisher_temperature = self.create_publisher(Temperature, 'vedrus/'+ self.side +'/temperature', 10)
-						self.publisher_humidity = self.create_publisher(RelativeHumidity, 'vedrus/'+ self.side +'/humidity', 10)
-
-						self.publisher_sonar = self.create_publisher(Sonar, 'vedrus/sonar', 10)
-
-						self.publisher_motor = self.create_publisher(Motor, 'vedrus/motor', 10)
-
-						self.publisher_raw = self.create_publisher(String, 'vedrus/'+ self.side +'/raw', 10)
-
-					if self.side is not None:
-						msg = String()
-						msg.data = line
-						self.publisher_raw.publish(msg)
-
-				if self.side is not None and len(div) == 14 and div[0] == self.start and div[ len(div) - 1 ] == 'END':
-
-					try:
-						#[0'VEDR', 1'196.88', 2'210.56', 3'0', 4'0', 5'2882', 6'-1', 7'0', 8'0', 9'0', 10'205.50', 11'0', 12'0', 'END']
-						stamp = self.get_clock().now().to_msg()
-
-						msg = Temperature()
-						msg.temperature = float(div[7])
-						msg.variance = 0.
-						self.publisher_temperature.publish(msg)
-
-						msg = RelativeHumidity()
-						msg.relative_humidity = float(div[8])
-						msg.variance = 0.
-						self.publisher_humidity.publish(msg)
-
-						msg = Sonar()
-						msg.header.frame_id = self.side +'_side'
-						msg.header.stamp = stamp
-						msg.range = -1. if div[5] == '-1' else float(div[5]) / 57.
-						msg.azimuth = 90. if self.side == 'right' else 270.
-						self.publisher_sonar.publish(msg)
-
-						msg = Sonar()
-						msg.header.frame_id = self.side +'_rear'
-						msg.header.stamp = stamp
-						msg.range = -1. if div[6] == '-1' else float(div[6]) / 57.
-						msg.azimuth = 180.
-						self.publisher_sonar.publish(msg)
-
-						msg = Motor()
-						msg.header.frame_id = self.side +'_front'
-						msg.header.stamp = stamp
-						msg.tick = int(div[3])
-						msg.current = float(div[1])
-						msg.power = int(div[11])
-						msg.forward = div[12] == '1'
-						self.publisher_motor.publish(msg)
-
-						msg = Motor()
-						msg.header.frame_id = self.side +'_rear'
-						msg.header.stamp = stamp
-						msg.tick = int(div[4])
-						msg.current = float(div[2])
-						msg.power = int(div[11])
-						msg.forward = div[12] == '1'
-						self.publisher_motor.publish(msg)
-
-					except ValueError:
-						print('error')
-						pass
-
-			self.buf = '' if self.side is not None and len(div) == 14 and div[0] == self.start and div[ len(div) - 1 ] == 'END' else lines[ len(lines) - 1 ]
-
+				pass
 
 def main(args=None):
 	rclpy.init(args=args)
