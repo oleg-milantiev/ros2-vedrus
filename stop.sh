7fc71151f69cc1cbd4b5a30d82a879c7c61a76ce
Oleg Milantiev <oleg@milantiev.com>
2024-03-14 04:13:09 +0000

    Stop engines! :)

---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /opt/ros/iron/
+source /opt/ros/iron/install/local_setup.bash
+source /opt/ros/iron/setup.bash
+#ros2 launch /opt/ros/iron/launch/yolo.py
+ros2 launch /opt/ros/iron/launch/ardu.py
