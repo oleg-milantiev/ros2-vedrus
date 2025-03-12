33e9dd9c7c2561d92f60e2973236fed03e6fdd8b
Oleg Milantiev <milantiev@octonus.com>
2025-03-12 22:50:37 +0300

    Docker with NVIDIA CUDA: Start to build on laptop

--- docker/ros2-apt/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build . -t ros2-apt
--- docker/rs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run -it --rm \
-    -v /dev:/dev \
-    --device-cgroup-rule "c 81:* rmw" \
-    --device-cgroup-rule "c 189:* rmw" \
-    librealsense/librealsense rs-depth
