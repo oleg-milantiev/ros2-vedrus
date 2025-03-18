126930811acb2f5e20d9e7731867cf32c8462a45
root <root@R2.milantiev.com>
2025-03-18 14:20:34 +0300

    Docker ROS2 with NVIDIA GPU

--- docker/nvidia.sh
+++ docker/nvidia.sh
@@ -2,3 +2,6 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt update
+apt-get install -y nvidia-container-toolkit
