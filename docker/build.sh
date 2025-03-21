319503128c8d5f4e2f96dfc845996c1141683a93
Oleg Milantiev <oleg@milantiev.com>
2025-03-21 11:18:55 +0300

    ROS2 Jazzy

---
+++ docker/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t olegmilantiev/docker-ros2-nvidia:cuda12.6.3-ubuntu24.04-ros2_jazzy .
--- docker/nvidia.sh
+++ docker/nvidia.sh
@@ -1,3 +1,9 @@
+#driver
+https://www.nvidia.com/en-us/drivers/details/242273/
+sh NVI*
+
+
+#container toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
@@ -5,3 +11,15 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
 
 apt update
 apt-get install -y nvidia-container-toolkit
+
+
+
+
+
+
+# не, это не то. Это CUDA-toolkit. А надо было container-toolkit
+#wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
+#sudo dpkg -i cuda-keyring_1.1-1_all.deb
+#sudo apt-get update
+#sudo apt-get -y install cuda-toolkit-12-6
+
